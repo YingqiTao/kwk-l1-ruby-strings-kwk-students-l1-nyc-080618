1218ecb3d65bc8155e0a66dfe0ad9fcbfec6010a
YingqiTao <yingqitao2000@gmail.com>
2018-08-07 14:52:41 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,3 @@
-# Code your prompts here!
+puts "Dear " + guest_name + ", You are cordially invited to the " + party_name + " on " + date + " at " + time + ". Please RSVP no later than October 30. Sincerely, " + host_name
 
 # Try starting out with puts'ing a string.
